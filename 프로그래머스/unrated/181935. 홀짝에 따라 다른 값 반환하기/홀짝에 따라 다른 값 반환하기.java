cb5d56fa051939741f85adcc1b793224e7753ce1
seulkimn <121601699+seulkimn@users.noreply.github.com>
2023-06-19 11:24:22 +0900

    [unrated] Title: 홀짝에 따라 다른 값 반환하기, Time: 0.02 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.java
+++ 프로그래머스/unrated/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.java
@@ -2,19 +2,12 @@ class Solution {
     public int solution(int n) {
         int answer = 0;
         
-        //n이 홀수면 n이하의 모든 홀수의 합 구하기.
-        
         if(n%2!=0) {
-        for(int i=0; i<=n; i++) { // n이하의 홀수의 합
-        	if(i%2!=0) 
-        		answer +=i; 
-        	
-        	}
-        }else if (n%2==0 ) {
-        for(int i=0; i<=n; i++) {	// n이하의 짝수의 합
-        	if(i%2 == 0) 
+        for(int i=1; i<=n; i+=2) 
+        	answer += i; 
+        }else
+        for(int i=0; i<=n; i+=2) {
         		answer += (int)Math.pow(i, 2);
-        	}
         }
         return answer;
     }
