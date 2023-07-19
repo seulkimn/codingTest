b2c2f72a11605b9e6f8ffddc91a18306eaed28f0
seulkimn <121601699+seulkimn@users.noreply.github.com>
2023-07-19 21:23:49 +0900

    [level 0] Title: 몫 구하기, Time: 0.02 ms, Memory: 74 MB -BaekjoonHub

--- 프로그래머스/lv0/120805. 몫 구하기/몫 구하기.java
+++ 프로그래머스/lv0/120805. 몫 구하기/몫 구하기.java
@@ -1,9 +1,13 @@
 class Solution {
     public int solution(int num1, int num2) {
+        boolean val = (0<num1 && num1 <=100 && 0< num2 && num2 <=100);
         int answer = 0;
-        
-        answer = num1 / num2 ; 
-        
+
+        if(val){
+            answer = num1/num2;
+        }else{
+            answer = -1;
+        }
         return answer;
     }
 }
