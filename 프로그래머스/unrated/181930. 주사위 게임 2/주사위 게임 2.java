bc8d244386b13fd8fd3ce42542b542b167696bd1
seulkimn <121601699+seulkimn@users.noreply.github.com>
2023-08-01 16:55:02 +0900

    [unrated] Title: 주사위 게임 2, Time: 6.28 ms, Memory: 84.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181930. 주사위 게임 2/주사위 게임 2.java
+++ 프로그래머스/unrated/181930. 주사위 게임 2/주사위 게임 2.java
@@ -1,17 +1,26 @@
 class Solution {
     public int solution(int a, int b, int c) {
-        int answer = 0;
-        double d = 0;
+        int answer = 1;
+
+        int count = 1;
+    
+        // 세 숫자 중 어느 두 숫자는 같고 나머지 숫자는 다르다면
+        if(a == b || a == c || b == c) {
+            count++;
+        }
+        System.out.println("두 숫자만 같을 때 = " + count);
+
+       // 세 수가 모두 같다면
+        if(a == b && b == c) {
+            count++;
+        }
+        System.out.println("세 수가 모두 같을 때 = " + count);
         
-        if(a==b&&b==c) {
-        	d = (a+b+c)*(Math.pow(a,2)+Math.pow(b, 2)+Math.pow(c, 2))*(Math.pow(a,3)+Math.pow(b, 3)+Math.pow(c, 3));
-        } else if(a==b||a==c||b==c) {
-        	d = (a+b+c)*(Math.pow(a,2)+Math.pow(b, 2)+Math.pow(c, 2));
-        }else
-        	d = a+b+c;
-        
-        answer = (int)d;
-        
+        for(int i = 1; i <= count; i++) {
+            answer *= (Math.pow(a,i)+Math.pow(b,i)+Math.pow(c,i));
+        }
+
         return answer;
     }
+
 }
