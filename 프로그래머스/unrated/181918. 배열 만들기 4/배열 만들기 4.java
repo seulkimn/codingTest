2c31a357b94de3ec6d4a4051b0b41370814397cb
seulkimn <121601699+seulkimn@users.noreply.github.com>
2023-08-02 15:12:51 +0900

    [unrated] Title: 배열 만들기 4, Time: 4.30 ms, Memory: 79.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181918. 배열 만들기 4/배열 만들기 4.java
+++ 프로그래머스/unrated/181918. 배열 만들기 4/배열 만들기 4.java
@@ -1,31 +1,34 @@
 import java.util.ArrayList;
 
-class Solution {
-    public int[] solution(int[] arr) {
-        ArrayList<Integer> tmp= new ArrayList();
-        
-        int i=0;
-        while(i<arr.length){
-            
-            if(tmp.size()==0){
-                tmp.add(arr[i]);
-                i=i+1;
-            }
-            else{
-                int last=tmp.get(tmp.size()-1);
-                if(tmp.size()!=0 && last<arr[i]){
-                tmp.add(arr[i]);
-                i=i+1;
-                }
-                else if(tmp.size()!=0 && last>=arr[i]){
-                    tmp.remove(tmp.size()-1);
-                }
-            }
-            
-        }
-        
-       int[] stk = tmp.stream().mapToInt(j -> j).toArray();
-        
-        return stk;
-    }
-}
+class Solution{
+	    public int[] solution(int[] arr) {
+	        ArrayList<Integer> tmp= new ArrayList();
+	        
+	        int i=0;
+	        while(i<arr.length){
+	            
+	            if(tmp.size()==0){
+	                tmp.add(arr[i]);
+	                i=i+1;
+	            }
+	            else if(tmp.size()!=0){
+	                int last=tmp.get(tmp.size()-1);
+	                if(last<arr[i]){ 
+	                tmp.add(arr[i]);
+	                i=i+1;
+	                }
+	                else if(last>=arr[i]){
+	                    tmp.remove(tmp.size()-1);
+	                }
+	            }
+	            
+	        }
+	        
+
+	        int stk[]=new int[tmp.size()];
+	        for(int j=0; j<tmp.size(); j++){
+	            stk[j]=tmp.get(j).intValue();
+	        }
+	        return stk;
+	    }
+	}
